1c2ef061513f51450b3d0d68e3e6885c7bacb877
Robin <robin.wujw@gmail.com>
2021-06-07 23:01:20 +0800

    002

--- 002.py
+++ 002.py
@@ -1,20 +1,60 @@
-class Solution(object):
-	def addTwoNumbers(self,l1,l2):
-	
+# class Solution(object):
+# 	def addTwoNumbers(self,l1,l2):
+# 		root = ListNode(0)		
+# 		result = root
+# 		carry = 0	
+# 		while l1 or l2 or carry == 1:
+# 			value = 0
+# 			if l1:
+# 					value += l1.val
+# 					l1 = l1.next
+# 			if l2:
+# 					value += l2.val
+# 					l2    = l2.next
+# 			value += carry
+# 			root.next = ListNode(value %10)
+# 			carry = int(value/10)
+# 			root = root.next
+# 		return result.next
 
-		root = ListNode(0)		
-		result = root
-		carry = 0	
-		while l1 or l2 or carry == 1:
-			value = 0
+#123+234 = 357
+#321+432 = 753 
+#673+587 = 
+# Definition for singly-linked list.
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+	def addTwoNumbers(self, l1, l2):
+		result = ListNode()
+		head = result
+		carry = 0
+		while l1 or l2 or carry==1:#l1 or l2值存在or存在进位(最高位进位)
+			value = 0 #value 表示result中某位的值
 			if l1:
-					value += l1.val
-					l1 = l1.next
+				value += l1.val
+				l1 = l1.next
 			if l2:
-					value += l2.val
-					l2    = l2.next
-			value += carry
-			root.next = ListNode(value %10)
-			carry = int(value/10)
-			root = root.next
-		return result.next
+				value += l2.val
+				l2 = l2.next
+			value += carry 
+			carry = int(value/10)# 进位
+			result.next = ListNode(value%10) #某位的值
+			result = result.next
+		return head.next
+if __name__ == "__main__":
+	So = Solution()
+	l1 = ListNode(4)
+	l1.next = ListNode(6)
+	l2 = ListNode(5)
+	l2.next = ListNode(6)
+	result = So.addTwoNumbers(l1,l2)
+	while(result):
+		print(result.val)
+		result = result.next
+
+
--- 003.py
+++ 003.py
@@ -1,11 +1,29 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self,s):
-        tempDict = {}
-        maxLen   = 0
-        pointer  = 0
+# class Solution(object):
+#     def lengthOfLongestSubstring(self,s):
+#         tempDict = {}
+#         maxLen   = 0
+#         pointer  = 0
+#         for index,value in enumerate(s):
+#             if value in tempDict:
+#                 pointer  = max(tempDict[value] +1 ,pointer)
+#             maxLen  =   max(index - pointer +1 ,maxLen)
+#             tempDict[value] = index
+#         return maxLen
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        Dict = {}
+        maxLen = 0
+        pointer = 0
         for index,value in enumerate(s):
-            if value in tempDict:
-                pointer  = max(tempDict[value] +1 ,pointer)
-            maxLen  =   max(index - pointer +1 ,maxLen)
-            tempDict[value] = index
-        return maxLen
+            if value in Dict:
+                pointer = max(Dict[value],pointer)
+            maxLen = max(index-pointer+1 ,maxLen)
+            Dict[value] = index
+        print(Dict)
+        print(maxLen)
+# aabcadwe 
+# 1. a:0 maxLen=1 2.pointer=0 maxLen=
+if __name__ == "__main__":
+    So = Solution()
+    s = "aa"
+    So.lengthOfLongestSubstring(s)
