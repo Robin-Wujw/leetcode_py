a5b912f17a64ef0008993ed67931c773405f42e0
Robin <robin.wujw@gmail.com>
2021-06-08 19:27:57 +0800

    003网友解法

--- 003.py
+++ 003.py
@@ -16,14 +16,33 @@ class Solution:
         pointer = 0
         for index,value in enumerate(s):
             if value in Dict:
-                pointer = max(Dict[value],pointer)
+                pointer = max(Dict[value]+1,pointer)
             maxLen = max(index-pointer+1 ,maxLen)
             Dict[value] = index
         print(Dict)
         print(maxLen)
+    def lengthOfLongestSubstring1(self, s: str) -> int:
+        if not s:return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+                print(i,s[i],cur_len,lookup,left)
+            print(cur_len)
+            if cur_len > max_len:max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+        
 # aabcadwe 
 # 1. a:0 maxLen=1 2.pointer=0 maxLen=
 if __name__ == "__main__":
     So = Solution()
-    s = "aa"
-    So.lengthOfLongestSubstring(s)
+    s = "rrasdsw"
+    So.lengthOfLongestSubstring1(s)
