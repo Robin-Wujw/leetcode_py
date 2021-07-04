753ef171add51ae369e5cf8d5bf6dd3c92453410
Robin <robin.wujw@gmail.com>
2021-07-04 19:52:09 +0800

    013

--- 009.py
+++ 009.py
@@ -1,25 +1,41 @@
-# coding=utf-8
-import sys
+# # coding=utf-8
+# import sys
+# class Solution:
+#     def isPalindrome(self, x):
+#         """
+#         :type x: int
+#         :rtype: bool
+#         """
+#         if x < 0 or (x ! = 0 and x%10 == 0):
+#             return False
+#         else:
+#             val = 0
+#             tem = x
+#             while(val < tem):
+#                 val = val * 10 + tem % 10
+#                 tem = tem // 10
+#             return val == tem or  val//10 == tem
+# def main():
+#     x = 10
+#     a = Solution()
+#     print(a.isPalindrome(x))
+# main()
 class Solution:
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x < 0 or (x ! = 0 and x%10 == 0):
+        if(x<0 or (x!=0 and x %10 ==0)):
             return False
+        num = 0 
+        x = str(x)
+        for i in range(int(len(x)/2)):
+            if(x[i]==x[-i-1]):
+                num = num+1
+        if num == int(len(x)/2):
+            return True
         else:
-            val = 0
-            tem = x
-            while(val < tem):
-                val = val * 10 + tem % 10
-                tem = tem // 10
-            return val == tem or  val//10 == tem
-def main():
-    x = 10
-    a = Solution()
-    print(a.isPalindrome(x))
-main()
+            return False
+if __name__ == "__main__":
+    x = Solution()
+    print(x.isPalindrome(1214431312))
 
 
 
@@ -35,4 +51,4 @@ main()
 
 
 
-\
+
--- 013.py
+++ 013.py
@@ -1,31 +1,46 @@
+# class Solution:
+#     def romanToInt(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         dict = {
+#             "I":1,
+#             "V":5,
+#             "X":10,
+#             "L":50,
+#             "C":100,
+#             "D":500,
+#             "M":1000,
+#         }
+#         result = 0
+#         for c in s:
+#             result += dict[c]
+#         if s.find("IV") != -1:
+#             result -=2
+#         if s.find("IX") != -1:
+#             result -=2
+#         if s.find("XL") != -1:
+#             result -=20
+#         if s.find("XC") != -1:
+#             result -=20
+#         if s.find("CD") != -1:
+#             result -=200
+#         if s.find("CM") != -1:
+#             result -=200
+#         return result
+
 class Solution:
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        dict = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L":50,
-            "C":100,
-            "D":500,
-            "M":1000,
-        }
-        result = 0
-        for c in s:
-            result += dict[c]
-        if s.find("IV") != -1:
-            result -=2
-        if s.find("IX") != -1:
-            result -=2
-        if s.find("XL") != -1:
-            result -=20
-        if s.find("XC") != -1:
-            result -=20
-        if s.find("CD") != -1:
-            result -=200
-        if s.find("CM") != -1:
-            result -=200
-        return result
+        dict = { "I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000 }
+        num = 0
+        for i in range(len(s)):
+            if i<len(s)-1 and dict[s[i]]<dict[s[i+1]]:
+                num-=dict[s[i]]
+            else:
+                num+=dict[s[i]]
+        return num 
+if __name__ == "__main__":
+    s = Solution()
+    print(s.romanToInt("MCMXCIV"))
+
