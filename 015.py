cf11798dfa2a82c22299a4d96a132c453bdd3a04
Robin_Wujw <robin.wujw@gmail.com>
2021-07-25 00:47:47 +0800

    015

--- 015.py
+++ 015.py
@@ -1,7 +1,6 @@
 class Solution:
     def threeSum(self,nums):
         """
-
         :type nums: List[int]
         :return: List[List[int]]
         n^2
@@ -24,26 +23,37 @@ class Solution:
                 elif total > 0:
                     right--
         """
+        # results = []
+        # nums.sort()
+        # for index,a in enumerate(nums):
+        #     if index >=1 and nums[index-1] == a:
+        #         continue
+        #     left,right = index+1,len(nums) - 1
+        #     while left<right:
+        #         b = nums[left]
+        #         c = nums[right]
+        #         total = a+b+c
+        #         if total <0:
+        #             left +=1
+        #         elif total>0:
+        #             right-=1
+        #         else:
+        #             results.append([a,b,c])
+        #             while left < right and nums[left] == nums[left+1]:
+        #                 left += 1
+        #             while left < right and nums[right] == nums[right-1]:
+        #                 right -= 1
+        #             left +=1
+        #             right-=1
+        # return results
         results = []
         nums.sort()
         for index,a in enumerate(nums):
-            if index >=1 and nums[index-1] == a:
-                continue
-            left,right = index+1,len(nums) - 1
-            while left<right:
+            
+            left = index + 1 
+            right = len(nums)-1
+            while left < right:
                 b = nums[left]
                 c = nums[right]
-                total = a+b+c
-                if total <0:
-                    left +=1
-                elif total>0:
-                    right-=1
-                else:
-                    results.append([a,b,c])
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                    left +=1
-                    right-=1
-        return results
+                total = a + b + c
+                if 
