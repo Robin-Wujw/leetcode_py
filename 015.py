dd4d124fedc0f2cb7897b40b250e5f5aa1b92c25
Robinwu <robin.wujw@gmail.com>
2021-07-28 15:50:02 +0800

    015

--- 015.py
+++ 015.py
@@ -49,11 +49,31 @@ class Solution:
         results = []
         nums.sort()
         for index,a in enumerate(nums):
-            
+            #选择a时候的去重
+            if index >=1 and nums[index-1] == a:
+               continue
             left = index + 1 
             right = len(nums)-1
             while left < right:
                 b = nums[left]
                 c = nums[right]
                 total = a + b + c
-                if 
+                if total<0:
+                    left = left+1
+                elif total>0:
+                    right = right-1
+                else:
+                    results.append([a,b,c])
+                    print(a,left,right)
+                    # a确定之后，后面的left和right指向的元素去重
+                    while left<right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left<right and nums[right] == nums[right-1]:
+                        right-=1
+                    left  += 1
+                    right -= 1
+        return results
+if __name__ == "__main__":
+    s = Solution()
+    nums = [-2,0,0,2,2]
+    print(s.threeSum(nums))
