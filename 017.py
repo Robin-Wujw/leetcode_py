ce7bcda0710824814072fb4a7fe9866d6956b990
Robin_Wujw <robin.wujw@gmail.com>
2021-08-07 14:41:07 +0800

    017回溯法

--- 017.py
+++ 017.py
@@ -1,33 +1,62 @@
-from  functools import reduce
+# class Solution(object):
+#     def letterCombinations(self,digits):
+#         """
 
-class Solution(object):
-    def letterCombinations(self,digits):
-        """
+#         :type digits:  str
+#         :return:       List{str}
+#         """
+#         keyMap = {
+#             "0": " ",
+#             "1": "*",
+#             "2": "abc",
+#             "3": "def",
+#             "4": "ghi",
+#             "5": "jkl",
+#             "6": "mno",
+#             "7": "pqrs",
+#             "8": "tuv",
+#             "9": "wxyz",
+#         }
+#         results = []
+#         for digit in digits:
+#             if len(results) == 0 :
+#                 for letter in keyMap[digit]:
+#                     results.append(letter)
+#             else:
+#                 tempResults = []
+#                 for result in results:
+#                     for letter in keyMap[digit]:
+#                         tempResults.append(result + letter)
+#                 results = tempResults
+#         return results
 
-        :type digits:  str
-        :return:       List{str}
-        """
-        keyMap = {
-            "0": " ",
-            "1": "*",
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+class Solution:
+    def letterCombinations(self, digits):
+        num_dic = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
-        results = []
-        for digit in digits:
-            if len(results) == 0 :
-                for letter in keyMap[digit]:
-                    results.append(letter)
-            else:
-                tempResults = []
-                for result in results:
-                    for letter in keyMap[digit]:
-                        tempResults.append(result + letter)
-                results = tempResults
-        return results
+        self.s = ""
+        res = []
+        if len(digits) == 0 : return res 
+        def backtrack(digits,index):
+            if index == len(digits): return res.append(self.s)
+            digit =digits[index]
+            letters = num_dic[digit]
+            for letter in letters:
+                self.s = self.s + letter
+                backtrack(digits,index+1)
+                self.s = self.s[:-1]
+        backtrack(digits,0)
+        return res
+
+if __name__ =="__main__":
+    s = Solution()
+    st = '423'
+    print(s.letterCombinations(st))
