01e0874d4e57fee842a1d2062a8b4ba487f9c2c8
Robin_Wujw <robin.wujw@gmail.com>
2021-08-16 16:56:11 +0800

    077组合 回溯法

---
+++ 077.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combine(self, n, k):
+        '''
+        给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+
+        你可以按 任何顺序 返回答案。
+
+        '''
+        l = []
+        res = []
+        def backtrack(n,k,startIndex):
+            if len(l) == k:
+                res.append(l[:])
+                return 
+            for i in range(startIndex,n+1):
+                l.append(i)
+                backtrack(n,k,i+1)
+                l.pop()
+        backtrack(n,k,1)
+        return res
+
+if __name__ ==  "__main__":
+    s = Solution()
+    n = 4
+    k = 2
+    print(s.combine(n,k))
