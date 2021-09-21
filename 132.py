a607ec5960c725edebc4132938ff509ed408d76b
Robin_Wujw <robin.wujw@gmail.com>
2021-09-21 17:54:01 +0800

    class Solution:

--- 132.py
+++ 132.py
@@ -4,36 +4,21 @@ class Solution:
         给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是回文。
         返回符合要求的 最少分割次数。
         '''
-        # path = []
-        # result = []
-        # def backtrack(s,startIndex):
-        #     if(startIndex>=len(s)):
-        #         return result.append(path[:])
-        #     for i in range(startIndex,len(s)):
-        #         l = s[startIndex:i+1]
-        #         if(l==l[::-1]):path.append(l[:])
-        #         else: continue
-        #         backtrack(s,i+1)
-        #         path.pop()
-        # backtrack(s,0)
-        # ret = 100000000
-        # for i in result:
-        #     ret = min(len(i)-1,ret)
-        # return ret
-        path = []
-        result = []
-        def backtrack(s,startIndex):
-            if(startIndex>=len(s)):
-                return result.append(path[:])
-            for i in range(startIndex,len(s)):
-                l = s[startIndex:i+1]
-                if(l==l[::-1]):path.append(l[:])
-                else: continue
-                backtrack(s,i+1)
-                path.pop()
-        backtrack(s,0)
-        return result
-
+        isPalindromic=[[False]*len(s) for _ in range(len(s))]
+        for i in range(len(s)-1, -1, -1): #注意遍历顺序
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (j - i <= 1 or isPalindromic[i+1][j-1]): 
+                    isPalindromic[i][j] = True
+        
+        dp = [i for i in range(len(s))]
+        for i in range(1,len(s)):
+            if(isPalindromic[0][i]):
+                dp[i] = 0
+                continue
+            for j in range(0,i):
+                if(isPalindromic[j+1][i]):
+                    dp[i] = min(dp[i],dp[j]+1)
+        return dp[-1]
 if __name__ ==  "__main__":
     s = Solution()
     a = "aab"
