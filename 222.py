a44001169105a69c112bb7a63f5cd3ae2b077cef
Robin_Wujw <robin.wujw@gmail.com>
2021-10-20 21:06:01 +0800

    队列-栈

--- 222.py
+++ 222.py
@@ -9,7 +9,7 @@ class Solution:
     给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
     完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
     """
-    # def countNodes(self, root):
+    # def countNodes(self, root):   
     #     return self.getNodes(root)
     # def getNodes(self,root):
     #     if(not root): return 0
