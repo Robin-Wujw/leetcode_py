a98ac285dbc4d5179911c11be138c3cbe48b1344
Robin_Wujw <robin.wujw@gmail.com>
2021-10-26 10:39:45 +0800

    242. 有效的字母异位词

--- 235.py
+++ 235.py
@@ -9,20 +9,19 @@ class Solution:
     百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
     [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
     """
-    # def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # if(root.val == p.val or root.val == q.val):
-        #     return root
-        # if(root.val > p.val and root.val < q.val or root.val < p.val and root.val > q.val):
-        #     return root 
-        #有bug 只返回了一层递归输出，结果并不一定。
-        # elif(root.val > p.val and root.val > q.val):
-        #     self.lowestCommonAncestor(root.left,p,q)
-        # elif(root.val < p.val and root.val < q.val):
-        #     self.lowestCommonAncestor(root.right,p,q)
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        if root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        return root
+        if(root.val == p.val or root.val == q.val):
+            return root
+        if(root.val > p.val and root.val < q.val or root.val < p.val and root.val > q.val):
+            return root 
+        elif(root.val > p.val and root.val > q.val):
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif(root.val < p.val and root.val < q.val):
+            return self.lowestCommonAncestor(root.right,p,q)
+
+    # def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    #     if root.val > p.val and root.val > q.val:
+    #         return self.lowestCommonAncestor(root.left, p, q)
+    #     if root.val < p.val and root.val < q.val:
+    #         return self.lowestCommonAncestor(root.right, p, q)
+    #     return root
---
+++ 242.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+        注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+        示例 1:
+        输入: s = "anagram", t = "nagaram"
+        输出: true
+        """
+        result1 = {}
+        for i in s:
+            if(i not in result1):
+                result1[i] += 1
+        for i in t:
+            if(i not in result1):
+                return False
+            else:
+                result1[i] -= 1
+        for i in result1.values:
+            if(i!=0):
+                return False
