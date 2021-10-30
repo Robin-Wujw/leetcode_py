8a7e860c85efc81157d8649fe171d88975169c13
Robin_Wujw <robin.wujw@gmail.com>
2021-10-30 11:12:52 +0800

    fix bugs

--- 014.py
+++ 014.py
@@ -44,6 +44,10 @@
 #         res = res + x[0]
 #     return res
 class Solution:
+    """
+    编写一个函数来查找字符串数组中的最长公共前缀。
+    如果不存在公共前缀，返回空字符串 ""。
+    """
     def longestCommonPrefix(self, strs):
         res = ""
         if len(strs)==0:
