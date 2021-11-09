506489db7de4a67bf01ac917fab1a6340c789ca7
Robin_Wujw <robin.wujw@gmail.com>
2021-11-09 09:29:19 +0800

    379.py

---
+++ 379.py
@@ -0,0 +1,35 @@
+class Solution:
+    def wiggleMaxLength(self, nums):
+        """
+        如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
+        例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的。
+        相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+        子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
+        给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度。
+        """
+        if(len(nums)<=1):return len(nums)
+        preDiff = 0
+        curDiff = 0 
+        result = 1 
+        for i in range(len(nums)-1):
+            curDiff = nums[i+1] - nums[i]
+            if((preDiff<=0 and curDiff>0) or (preDiff >=0 and curDiff<0)):
+                result += 1
+                preDiff = curDiff
+        return result
+        dp = [[0]*2 for i in range(len(nums))]
+        def wiggleMaxLength(self, nums):
+            dp[0][0] = dp[0][1] = 1
+            for i in range(1,len(nums)):
+                dp[i][0] = dp[i][1] = 1
+                for j in range(0,i):
+                    if (nums[j] > nums[i]):
+                        dp[i][1] = max(dp[i][1], dp[j][0] + 1)
+                for j in range(0,i):
+                    if (nums[j] < nums[i]):
+                         dp[i][0] = max(dp[i][0], dp[j][1] + 1)
+            return max(dp[nums.size() - 1][0], dp[nums.size() - 1][1])
+if __name__ ==  "__main__":
+    a = Solution()
+    nums = [1,17,5,10,13,15,10,5,16,8]
+    print(a.wiggleMaxLength(nums))
