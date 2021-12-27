807b80be182a6478cb85eaeb1d2bfa1f75907c7a
Robin_Wujw <robin.wujw@gmail.com>
2021-12-27 18:32:03 +0800

    补, 提交人: Robin, 提交时间: 2021-12-27 18:32:01

---
+++ 825.适龄的朋友.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=825 lang=python3
+#
+# [825] 适龄的朋友
+#
+
+# @lc code=start
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int:
+        pass
+# @lc code=end
+
