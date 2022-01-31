39e496ec4b85e2f4f6300bed185e4a53d0e39248
Robin_Wujw <robin.wujw@gmail.com>
2022-02-01 01:15:27 +0800

    更新, 提交人: Robin, 提交时间: 2022-02-01 01:15:25

--- 1342.将数字变成-0-的操作次数.py
+++ 1342.将数字变成-0-的操作次数.py
@@ -7,16 +7,23 @@
 # @lc code=start
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        count = 0
-        while num!=0:
-            if(num % 2 == 0):
-                num = num / 2 
-                count += 1 
-                continue 
-            else:
-                num = num - 1
-                count += 1 
-                continue
-        return count
+        # count = 0
+        # while num!=0:
+        #     if(num % 2 == 0):
+        #         num = num / 2 
+        #         count += 1 
+        #         continue 
+        #     else:
+        #         num = num - 1
+        #         count += 1 
+        #         continue
+        # return count
+        ans = 0
+        while num:
+            ans += num & 1
+            if num > 1:
+                ans += 1
+            num >>= 1
+        return ans
 # @lc code=end
 
---
+++ 1763.最长的美好子字符串.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=1763 lang=python3
+#
+# [1763] 最长的美好子字符串
+#
+
+# @lc code=start
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        n = len(s)
+        maxPos, maxLen = 0, 0
+        for i in range(n):
+            lower, upper = 0, 0
+            for j in range(i, n):
+                if s[j].islower():
+                    lower |= 1 << (ord(s[j]) - ord('a'))# 通过二进制判断 那些位数出现过小写or大写（因为只有26个字母）
+                else:
+                    upper |= 1 << (ord(s[j]) - ord('A'))
+                if lower == upper and j - i + 1 > maxLen:
+                    maxPos = i
+                    maxLen = j - i + 1
+        return s[maxPos: maxPos + maxLen]
+# @lc code=end
+
