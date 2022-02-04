88ad534de3128052afbae4e89ab2ed12a3c99599
Robin_Wujw <robin.wujw@gmail.com>
2022-02-04 23:56:19 +0800

    更新, 提交人: Robin, 提交时间: 2022-02-04 23:56:16

---
+++ 1725.可以形成最大正方形的矩形数目.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=1725 lang=python3
+#
+# [1725] 可以形成最大正方形的矩形数目
+#
+
+# @lc code=start
+class Solution:
+    def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+        res, maxLen = 0, 0
+        for l, w in rectangles:
+            k = min(l, w)
+            if k == maxLen:
+                res += 1
+            elif k > maxLen:
+                res = 1
+                maxLen = k
+        return res
+# @lc code=end
+
