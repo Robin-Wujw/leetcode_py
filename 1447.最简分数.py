e422268d01ec91d59799a5823e82f108a928b3f0
Robin_Wujw <robin.wujw@gmail.com>
2022-02-10 01:25:44 +0800

    更新, 提交人: Robin, 提交时间: 2022-02-10 01:25:41

---
+++ 1447.最简分数.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1447 lang=python3
+#
+# [1447] 最简分数
+#
+
+# @lc code=start
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        pass
+# @lc code=end
+
