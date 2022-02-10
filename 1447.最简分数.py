7ba47b2e02cea1610bb76c1e47c76547c5a67cf3
Robin_Wujw <robin.wujw@gmail.com>
2022-02-11 01:45:45 +0800

    打卡, 提交人: Robin, 提交时间: 2022-02-11 01:45:39

--- 1447.最简分数.py
+++ 1447.最简分数.py
@@ -7,6 +7,6 @@
 # @lc code=start
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
-        pass
+        return [f"{numerator}/{denominator}" for denominator in range(2, n + 1) for numerator in range(1, denominator) if gcd(denominator, numerator) == 1]
 # @lc code=end
 
