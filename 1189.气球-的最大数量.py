2f69b3fb934723fbf8ea49df7ac4b8758af078ad
Robin_Wujw <robin.wujw@gmail.com>
2022-02-13 01:42:47 +0800

    打卡, 提交人: Robin, 提交时间: 2022-02-13 01:42:44

---
+++ 1189.气球-的最大数量.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=1189 lang=python3
+#
+# [1189] “气球” 的最大数量
+#
+
+# @lc code=start
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        dict = {}
+        ret  = 100000
+        for i in text:
+            if(i not in dict):
+                dict[i] = 1
+            else:
+                dict[i] += 1 
+        
+        for i in dict:
+            if('b' not in dict or 'a' not in dict or 'l' not in dict or 'o' not in dict or 'n' not in dict ):
+                return 0
+            if(i=='b' or i=='a' or i=='n'):
+                ret = min(ret,dict[i])
+            elif(i=='l' or i=='o'):
+                ret = min(ret,int(dict[i]/2))
+        return ret 
+
+# @lc code=end
+
