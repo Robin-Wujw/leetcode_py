a64914acf1bfba0cb5e14308c81d0a31bd16e131
Robin_Wujw <robin.wujw@gmail.com>
2022-02-13 01:43:36 +0800

    官方题解, 提交人: Robin, 提交时间: 2022-02-13 01:43:32

--- 1189.气球-的最大数量.py
+++ 1189.气球-的最大数量.py
@@ -7,22 +7,29 @@
 # @lc code=start
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        dict = {}
-        ret  = 100000
-        for i in text:
-            if(i not in dict):
-                dict[i] = 1
-            else:
-                dict[i] += 1 
+        # dict = {}
+        # ret  = 100000
+        # for i in text:
+        #     if(i not in dict):
+        #         dict[i] = 1
+        #     else:
+        #         dict[i] += 1 
         
-        for i in dict:
-            if('b' not in dict or 'a' not in dict or 'l' not in dict or 'o' not in dict or 'n' not in dict ):
-                return 0
-            if(i=='b' or i=='a' or i=='n'):
-                ret = min(ret,dict[i])
-            elif(i=='l' or i=='o'):
-                ret = min(ret,int(dict[i]/2))
-        return ret 
+        # for i in dict:
+        #     if('b' not in dict or 'a' not in dict or 'l' not in dict or 'o' not in dict or 'n' not in dict ):
+        #         return 0
+        #     if(i=='b' or i=='a' or i=='n'):
+        #         ret = min(ret,dict[i])
+        #     elif(i=='l' or i=='o'):
+        #         ret = min(ret,int(dict[i]/2))
+        # return ret 
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        cnt = Counter(ch for ch in text if ch in "balon")
+        cnt['l'] //= 2
+        cnt['o'] //= 2
+        return min(cnt.values()) if len(cnt) == 5 else 0
+
 
 # @lc code=end
 
