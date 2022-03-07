7e0259486f7e7921e9cc7a355c12eb3004fdf358
Robin_Wujw <robin.wujw@gmail.com>
2022-03-08 00:32:19 +0800

    补卡, 提交人: Robin, 提交时间: 2022-03-08 00:32:12

---
+++ 504.七进制数.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=504 lang=python3
+#
+# [504] 七进制数
+#
+
+# @lc code=start
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        negative = num < 0
+        num = abs(num)
+        digits = []
+        while num:
+            digits.append(str(num % 7))
+            num //= 7
+        if negative:
+            digits.append('-')
+        return ''.join(reversed(digits))
+
+# @lc code=end
+
