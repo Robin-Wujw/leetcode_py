b72ffd539ea25d0fe383c081f437c4cda42425a7
Robin_Wujw <robin.wujw@gmail.com>
2022-03-25 15:56:06 +0800

    打卡, 提交人: Robin, 提交时间: 2022-03-25 15:55:58

---
+++ 172.阶乘后的零.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=172 lang=python3
+#
+# [172] 阶乘后的零
+#
+
+# @lc code=start
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        ans = 0
+        for i in range(5, n + 1, 5):
+            print(i)
+            while i % 5 == 0:
+                i = i//5
+                ans += 1
+        return ans
+
+# @lc code=end
+
