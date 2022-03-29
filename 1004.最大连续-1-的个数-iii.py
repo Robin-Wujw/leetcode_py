953d061ffcfd53ed2853f6c9a09442edbd5b4f81
Robin_Wujw <robin.wujw@gmail.com>
2022-03-29 11:47:35 +0800

    打卡, 提交人: Robin, 提交时间: 2022-03-29 11:47:30

--- 1004.最大连续-1-的个数-iii.py
+++ 1004.最大连续-1-的个数-iii.py
@@ -7,13 +7,25 @@
 # @lc code=start
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        ans,sum,left = 0,0,0
-        for right in range(len(nums)):
-            sum += nums[right] != 1
-            while(sum>k):
-                sum -= nums[left] != 1
-                left += 1 
-            ans = max(ans,right-left+1)
-        return ans 
+        # ans,sum,left = 0,0,0
+        # for right in range(len(nums)):
+        #     sum += nums[right] != 1
+        #     while(sum>k):
+        #         sum -= nums[left] != 1
+        #         left += 1 
+        #     ans = max(ans,right-left+1)
+        # return ans
+        n = len(nums)
+        P = [0]
+        for num in nums:
+            P.append(P[-1] + (1 - num))
+        
+        ans = 0
+        for right in range(n):
+            left = bisect.bisect_left(P, P[right + 1] - k)
+            ans = max(ans, right - left + 1)#前缀和之差
+        
+        return ans
+
 # @lc code=end
 
